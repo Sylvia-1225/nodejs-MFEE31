10eaeb09565e50f49bbe433ca78356dab60914c2
sylvia <iloveyui0928@gmail.com>
2022-12-10 15:12:44 +0800

    add readfile.js ver.2

--- js/readfile.js
+++ js/readfile.js
@@ -12,16 +12,21 @@ const fs = require('fs');
 //   }
 // });
 
+// Promise 是一個表示非同步運算的最終完成或失敗的物件。
 let p = new Promise((resolve, reject) => {
     fs.readFile('test.txt', 'utf-8', (err, data) => {
         if (err) {
+            // 如果 err 有值，表示有錯誤發生
+            // 這裡應該要處理錯誤
             reject(err)
         } else {
+            // 進來這裡，表示 err 是空的 (可能是 null)
             resolve(data)
         }
     });
 })
 
+// 真正使用的人
 p.then((data) => {
     console.log('成功讀到資料', data);
   }).catch((error) => {
