9eb27e522fe35f7ed4a1ae2973fdde16bf12e0ab
sylvia <iloveyui0928@gmail.com>
2022-12-11 15:46:54 +0800

    add crawler2.js ver.finall

--- crawler/crawler2.js
+++ crawler/crawler2.js
@@ -1,24 +1,25 @@
+// axios await 版本
+// 把 query string 抽出來當變數，用 params 的方式去設定
+
 // 1. 安裝 npm i axios
 // 2. 引用 require
 // 3. 去讀官方文件
 const axios = require('axios');
-
 // http://54.71.133.152:3000/stocks?stockNo=2618&date=202211
-(async function () {
-    const response = await axios.get('http://54.71.133.152:3000/stocks?stockNo=2618&date=202211')
-    .then((response) => {
-        console.log(response.data);
-    })
-    .catch((e) => {
+
+(async () => {
+    try {
+        let stockNo = '2618';
+        let date = '20221111';
+        let response = await axios.get(`http://54.71.133.152:3000/stocks`, {
+        params: {
+            stockNo,
+            date,
+        },
+        });
+
+        console.log('await', response.data);
+    } catch (e) {
         console.error(e);
-    });
+    }
 })();
-
-
-// var asyncBtn = document.getElementById('asyncBtn');
-// asyncBtn.addEventListener('click', async function () {
-//   responseDom.innerText = '開始 axios';
-//   // http://54.71.133.152:3000/
-//   let response = await axios.get('http://54.71.133.152:3000/');
-//   responseDom.innerText = `來自 await axios 的結果 ${response.data}`;
-// });
